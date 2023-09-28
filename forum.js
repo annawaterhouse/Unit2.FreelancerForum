819823093c0d104e78b470b667c8f7e1f3be78af
annawaterhouse <annawaterhouse05@gmail.com>
2023-09-28 18:28:55 -0400

    changes

--- forum.js
+++ forum.js
@@ -1,5 +1,11 @@
+document.body.style.textAlign = "center";
 
-const freelancers = [
+const names = ["Alice", "Bob", "Carol", "David", "Eve", "Freddie"];
+const jobs = ["Writer", "Teacher", "Programmer", "Marketer", "Teacher", "Coder"];
+const prices = [30, 60, 70, 80, 90, 100];
+const maxListing = 10;
+
+const people = [
     {
         name: "Alice",
         occupation: "Writer",
@@ -17,23 +23,96 @@ const freelancers = [
     },
    
 ];
+//create text to dynamically display the average of the prices
+
+
+function averagePrice() {
+    let sum = 0;
+    for(const item of prices) {
+        sum += item;
+    }
+    return sum/prices.length;
+}
+
+function addP() {
+    const freelancers = document.querySelector("h3");
+    const p = document.createElement('p');
+    p.textContent = "This is fun";
+    freelancers.parentNode.insertAfter(p, )
+}
+
+
+
+
+function populateTable() {
+    // Get a reference to the table's tbody
+    const tableBody = document.querySelector("#myTable tbody");
+    tableBody.style.textAlign = "center";
+   
+    
+
+    // Use the map function to create table rows for each object
+    const rows = people.map((person) => {
+        
+        const row = document.createElement("tr");
+
+        // Create table data cells for each property
+        const nameCell = document.createElement("td");
+        const jobCell = document.createElement("td");
+        const priceCell = document.createElement("td");
+
+        // Set the text content of the table cells
+        nameCell.textContent = person.name;
+        jobCell.textContent = person.occupation;
+        priceCell.textContent = person.price;
+
+        nameCell.style.textAlign = "left";
+        jobCell.style.textAlign = "right";
+        priceCell.style.textAlign = "right";
+
+        // Append the table cells to the table row
+        row.appendChild(nameCell);
+        row.appendChild(jobCell);
+        row.appendChild(priceCell);
+
+        return row; // Return the created table row
+    });
+
+    // Append the rows to the table's tbody
+    // rows.forEach(function(row) {
+    //     tableBody.appendChild(row);
+    // });
+    tableBody.append(...rows);
+}
+
+//add more listings 
+function addPeople () {
+    const name = names["Alice", "Bob", "Carol", "David", "Eve", "Freddie"];
+    const occupation = jobs["Writer", "Teacher", "Programmer", "Marketer", "Teacher", "Coder"];
+    const price = prices[30, 60, 70, 80, 90, 100];
+
+    people.push({
+        name, occupation, price
+    })
+
+    populateTable();
+
+    if (people.length === maxListing) {
+        clearInterval(addPeopleIntervalID);
+    }
+}
+
+
+
+
+
+
+const addPeopleIntervalID = setInterval(addPeople, 1000);
+// Call the populateTable function to initially populate the table
+populateTable();
 
 //
 
 
 
-function createRows() {
-    const tableBody = document.querySelector("table tbody");
-    const rows = document.createElement("tr");
-    const dataElements = freelancers.map((freelancer) => {
-        const element = document.createElement("tr");
-        element.textContent(freelancer.name);
-        element.createTextNode("name");
-        return element;
-    });
 
-    rows.appendChildren(...dataElements);
-
-}
-
-createRows();
